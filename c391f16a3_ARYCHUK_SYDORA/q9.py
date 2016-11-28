391b8f6a3a9ffa3997924bdad7b5a4ec48849b1a
sarychuk <sarychuk@ualberta.ca>
2016-11-27 21:42:33 -0700

    removed Makefile and updated some of q9.py

--- c391f16a3_ARYCHUK_SYDORA/q9.py
+++ c391f16a3_ARYCHUK_SYDORA/q9.py
@@ -43,29 +43,95 @@ import sqlite3
 
 def main():
 
+	prefixdict = {}
+	sqlStr = ""
+	selectStr = ""
+	selectStart = False
+	selectAll = False
+	firstTriple = True
+	subjPredObj = []
+	strLen = 0
+	fromClause = "FROM rdf_triple tri"
+
 	if len(sys.argv) != 3:
 		print("Expected 3 args: q9.py <database file> <path to a file with a SPARQL query>")
 		sys.exit(2)
 
 	dbConn = sqlite3.connect(sys.argv[1])
 	queryFile = open(sys.argv[2], "r")
+
+	#sample select SPARQL Query
+	#PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+	#PREFIX schema: <http://schema.org/>
+	#PREFIX dbr: <http://dbpedia.org/resource/>
+	#PREFIX dbo: <http://dbpedia.org/ontology/>
+	#SELECT * WHERE {
+    	#?city rdf:type schema:City .
+  		#?city dbo:country dbr:Canada .
+	#} 
 	
-	print("files opened")
-
 	# Use dbConn.execute() to create the execution statement
+	for line in queryFile:
+		splitLine = line.split()
+		if(splitLine):
+			strLen = len(splitLine)
+			if(splitLine[0] == "PREFIX"):
+				prefixdict[splitLine[1][0:-1]] = splitLine[2][1:-1]
+			elif(splitLine[0] == "SELECT"):
+				selectStart = True
+				selectStr += splitLine[0] + " "
+				if(splitLine[1] == "*"):
+					selectAll = True
 
-	# Returning the rows of a select statement using a cursor
-	#cursor = conn.execute("SELECT id, name, address, salary  from COMPANY")
-	#for row in cursor:
-	#	print "ID = ", row[0]
-	#	print "NAME = ", row[1]
-	#	print "ADDRESS = ", row[2]
-	#	print "SALARY = ", row[3], "\n"
+					#temp change back
+					selectStr += "subject" + " "
+
+					#add from tables 
+					selectStr += fromClause + " "
+
+					#add where clause
+					selectStr += splitLine[2] + " "
+
+				else:
+					#handle individual items
+					print(splitLine[strLen - strLen] + " " + splitLine[strLen - strLen + 1] + " " + splitLine[strLen - strLen + 2] + " " + splitLine[strLen - strLen + 3])
+
+			elif(selectStart == True):
+				#check for curly braces to read through again?
+				#query commands
+				if (splitLine[0] == "}"):
+					tempStr = sqlStr
+					sqlStr = tempStr[0:-10]
+					continue
+				subjPredObj.append(splitLine)
+				sqlStr += selectStr
+				if ("http" in splitLine[1]):
+					sqlStr += "tri.predicate = \"" + splitLine[1] + "\" "
+				else:
+					predSplit = splitLine[1].split(":")
+					sqlStr += "tri.predicate = \"" + prefixdict[predSplit[0]] +  predSplit[1] + "\" "
+				sqlStr += "AND "
+				if (":" in splitLine[2]):
+					prefixInObject = True
+					objSplit = splitLine[2].split(":")
+				if (prefixInObject):
+					sqlStr += "tri.object = \"" + prefixdict[objSplit[0]] +  objSplit[1] + "\" " 
+				else:
+					sqlStr += "tri.object = \"" + splitLine[2] + "\" " 
+				sqlStr += "INTERSECT "
+				prefixInObject = False
+				
+	#for key, value in prefixdict.items():
+	#	print(key + " = " + value)
+
+	print()
+	print(sqlStr)
+	print()
+	print(subjPredObj)
+	print()
 
 	dbConn.close()
 	queryFile.close()
-
-	print("files closed")
 
 if __name__ == "__main__":
 	main()
